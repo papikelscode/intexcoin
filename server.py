7b766e8801e5086c496dd41e908fe4ec52b8f3e2
papikelscode <nwakanmakelechi8@gmail.com>
2022-03-21 15:42:16 +0100

    the count down calculation fixed

--- server.py
+++ server.py
@@ -380,7 +380,7 @@ def database():
     
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8050, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
 # if __name__ == '__main__':
 #     manager.run()
 #     app.run(host='127.0.0.1', port=8050, debug=True)
--- static/assets/js/count.js
+++ static/assets/js/count.js
@@ -1,132 +1,115 @@
-function Run(){
- 
-  var two = document.getElementsByClassName("two")
-  var five = document.getElementsByClassName("five")
-  var twenty = document.getElementsByClassName("twenty")
-  var thirty = document.getElementsByClassName("thirty")
-  var one = document.getElementsByClassName("one")
-  var twox = document.getElementsByClassName("twox")
 
+function starttwo(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
+        display.textContent = minutes + ":" + seconds;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function timer(){
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-
-  // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (2000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML =  hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
-
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
 }
 
+window.onload = function () {
+    var twoMinutes = 60 * 2,
+        display = document.querySelector('#demo');
+    starttwo(twoMinutes, display);
+};
 
+function startfive(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
+        display.textContent = minutes + ":" + seconds;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   if (s == two){
-//     var end = new Date('02/25/2021 00:00 AM' );
-
-//   }else if(s == five ){
-//     var end = new Date('02/22/2021 6:30 AM');
-//   }else if(s == twenty ){
-//     var end = new Date('02/20/2021 9:30 AM');
-//   }else if(s == thirty ){
-//     var end = new Date('02/19/2021 12:30 AM');
-//   }else if(s == one ){
-//     var end = new Date('02/15/2021 2:30 AM');
-//   }
-//   else if(s == twox ){
-//     var end = new Date('02/10/2021 5:30 AM');
-//   }
-
-// var _second = 1000
-// var _minute = _second * 60;
-// var _hour = _minute *60;
-// var timer;
-
-// function showRemaining(){
-//   var now = new Date();
-//   var distance = end - now;
-   
-//   if (distance < 0){
-//     clearInterval(timer);
-//     document.getElementById("demo").innerHTML= "its out already";
-//     return;
-//   }
-//   var hour = Math.floor((distance % _hour) / _hour);
-//   var minutes = Math.floor((distance % _hour) / _minute);
-//   var seconds = Math.floor((distance % _minute)/ _second);
-  
-
-//   document.getElementById('demo').innerHTML = hour +'hour,'+ minutes + 'minutes,' + seconds + 'seconds.';
-// }
-// timer = setInterval(showRemaining, 1000);
-// if ( s == 1){
-//   document.getElementById('demo').innerHTML= "Dont select nothing";
-//   end;
-// }
-
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
 }
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#demo');
+    startfive(fiveMinutes, display);
+};
+
+function starttwenty(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var twentyMinutes = 60 * 20,
+        display = document.querySelector('#demo');
+    starttwenty(twentyMinutes, display);
+};
+function startthirty(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var thirtyMinutes = 60 * 30,
+        display = document.querySelector('#demo');
+    startthirty(thirtyMinutes, display);
+};
+
+function startone(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var oneMinutes = 60 * 60,
+        display = document.querySelector('#demo');
+
+    startone(oneMinutes, display);
+};
